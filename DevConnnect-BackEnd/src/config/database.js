beb5cd8430c1e8f9a5bf065c7ad07dc8bbb21d10
Uttam Barve <uttambarve007@gmail.com>
2025-10-20 15:49:11 +0530

    added .env

--- DevConnnect-BackEnd/src/config/database.js
+++ DevConnnect-BackEnd/src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://uttambarve:pass123@devconnect.t1tkocb.mongodb.net/DevConnect");
+    await mongoose.connect(import.meta.env.MONGODB_URI);
 
 }
 
